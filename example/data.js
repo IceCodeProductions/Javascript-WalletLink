d2c151ae79c4b50882a03709cfd8e6dc489ffee8
Rob Helmstetter <rob@icecodeproductions.com>
2022-03-04 07:57:59 -0500

    Initial Setup
    Setting up the Repo with the required files

---
+++ example/data.js
@@ -0,0 +1,5 @@
+
+const APP_NAME = 'My Awesome DApp'
+const APP_LOGO_URL = 'https://example.com/logo.png'
+const ETH_JSONRPC_URL = 'https://mainnet.infura.io/v3/84842078b09946638c03157f83405213'
+const CHAIN_ID = 1
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require("webpack")
+const path = require("path")
+const {env} = process
+
+module.exports = {
+  target: "web",
+  mode: "production",
+  entry: "./src/app.js",
+  resolve: {
+    fallback: {
+        buffer: require.resolve('buffer/'),
+        fs : false,
+        "stream": require.resolve("stream-browserify")
+    },
+  },
+  output: {
+    filename: "walletlink-bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(env.NODE_ENV) || JSON.stringify('production'),
+        WALLETLINK_URL: JSON.stringify(env.WALLETLINK_URL),
+        WALLETLINK_VERSION: JSON.stringify(require("./package.json").version)
+      },
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+  }),
+  ],
+}
